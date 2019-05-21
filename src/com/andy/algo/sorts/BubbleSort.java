ee0cc70e19b36b0f1a8e10ddd1a50c671ac919d9
andytan <tjl@coinsea.pro>
2019-05-21 09:49:14 +0800

    init

---
+++ src/com/andy/algo/sorts/BubbleSort.java
@@ -0,0 +1,8 @@
+package com.andy.algo.sorts;
+
+/**
+ * 冒泡排序.
+ */
+public class BubbleSort {
+
+}
---
+++ src/com/andy/algo/sorts/InsertionSort.java
@@ -0,0 +1,8 @@
+package com.andy.algo.sorts;
+
+/**
+ * 插入排序.
+ */
+public class InsertionSort {
+    
+}
